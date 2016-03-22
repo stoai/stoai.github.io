9775ea5a0e280b62eeccadabf4cb884ef075a055
stoai <dinhtoai94@gmail.com>
2016-03-22 18:02:39 +0700

    update Slider v2

--- javascript/Slider/js/script.js
+++ javascript/Slider/js/script.js
@@ -1,109 +1,155 @@
-function moveElement(elementID, final_x, final_y, interval) { 
-	var elem = document.getElementById(elementID);
-	if (elem.movement) 
-		clearTimeout(elem.movement); 
+/**
+* The moveElement() function includes 4 parameters
+*
+*
+*/
 
-	// Xác định vị trí
+function moveElement(elementID, final_x, final_y, interval) {
+	var elem = document.getElementById(elementID);
+	if (elem.movement) {
+		clearTimeout(elem.movement);
+	}
+
+	// Determine position
 	var xpos = parseInt(elem.style.left);
 	var ypos = parseInt(elem.style.top);
 
-	// Sau khi dịch chuyển tới vị trí thì dừng
-	if (xpos == final_x && ypos == final_y)
+	// After move to position then stop.
+	if (xpos == final_x && ypos == final_y) {
 		return true;
+	}
 
-	// Tính vị trí dịch chuyển sau mỗi thời gian dừng
+	// Compute movement position after each time stop.
 	if (xpos < final_x) {
 		var dist = Math.ceil((final_x - xpos) / 10);
 		xpos = xpos + dist;
 	}
-	if (xpos > final_x) { 
+
+	if (xpos > final_x) {
 		var dist = Math.ceil((xpos - final_x) / 10);
 		xpos = xpos - dist;
 	}
-	if (ypos < final_y) { 
+
+	if (ypos < final_y) {
 		var dist = Math.ceil((final_y - ypos) / 10);
 		ypos = ypos + dist;
 	}
+
 	if (ypos > final_y) {
-		var dist = Math.ceil((ypos - fina_y) / 10);
+		var dist = Math.ceil((ypos - final_y) / 10);
 		ypos = ypos - dist;
 	}
 
-	// Di chuyển phần tử 
+	// Move elenment.
 	elem.style.left = xpos + "px";
 	elem.style.top = ypos + "px";
 
-	// Gọi lại hàm dịch chuyển sau khi dịch được 1 px 
+	// Invoked move function after moved 1px
 	var repeat = "moveElement('" + elementID + "'," + final_x + ", " + final_y + ", " + interval + ")";
-	elem.movement = setTimeout(repeat, interval); 
+	elem.movement = setTimeout(repeat, interval);
+	track();
 }
 
 /*==================== End moveElement() function ====================*/
 
-function previous() { 
-	if (move < 0) 
-	move += box_width;
+/**
+* The previous() function is...
+*
+*
+*/
+function previous() {
+	if (move < 0) {
+		move += box_width;
+	}
 	moveElement('slider', move, 0, 10);
+	track();
 }
 
-/*==================== End previous() function ====================*/ 
+/*==================== End previous() function ====================*/
 
+/**
+* The next() function is...
+*
+*
+*/
 function next() {
 	move = (move <= endpos) ? 0 : (move - box_width);
 	moveElement('slider', move, 0, 10);
+	track();
 }
 
 /*==================== End next() function ====================*/
 
 function track() {
-	
+	var items = document.getElementsByClassName("item");	
+	for (var i = 0; i < items.length; i++) {
+		if (i == move) {
+			items[move].style.background = "red";
+			break;
+		}else {
+			items[move].style.background = "green";
+		}
+	}
 }
 
 /*==================== End track() function ====================*/
-function slider() {
-	if (!document.getElementById) 
-		return false; 
-		var slider = document.getElementById('slider'); 
-		var wrap_slider = document.getElementById('wrap-slider'); 
-	
-	// Định kiểu cho slider nếu chưa được thiết lập
-	if (!slider.style.position) 
-		slider.style.position = "absolute";
-	if (!slider.style.left) 
-		slider.style.left = "0px";
-	if (!slider.style.top)
-		slider.style.top = "0px";
 
-	// Thiết lập các thông số cho slider
-	var box_arr = slider.childNodes; 
+/**
+* The slider() function is...
+*
+*
+*/
+function slider() {
+	if (!document.getElementById) {
+		return false;
+	}
+
+	var slider = document.getElementById('slider');
+	var wrap_slider = document.getElementById('wrap-slider');
+
+	// Set type for slider when it isn't set.
+	if (!slider.style.position) {
+		slider.style.position = "absolute";
+	}
+
+	if (!slider.style.left) {
+		slider.style.left = "0px";
+	}
+
+	if (!slider.style.top) {
+		slider.style.top = "0px";
+	}
+
+	// Setting parameters for slider
+	var box_arr = slider.childNodes;
 	var box_quantity = 0;
-	for (var i=0; i < box_arr.length; i++) {
+	for (var i = 0; i < box_arr.length; i++) {
 		if (box_arr[i].className == 'slide-box') {
-			box_quantity++; 
+			box_quantity++;
 			box_width = box_arr[i].offsetWidth;
 			box_height = box_arr[i].offsetHeight;
-		} 
+		}
 	}
-	 
-	wrap_slider.style.height = box_height + "px"; 
-	wrap_slider.style.width = box_width + "px"; 
-	slider.style.width = (box_width * box_quantity) + "px"; 
+
+	wrap_slider.style.height = box_height + "px";
+	wrap_slider.style.width = box_width + "px";
+	slider.style.width = (box_width * box_quantity) + "px";
 	move = 0;
 	endpos = -(box_width * 6);
- 
-	// Tự động chạy slider 
+
+	// Automatically run slider
 	var idSet = setInterval('next()', 3000);
- 
-	// next_slide , prev_slide 
+
+	// next_slide , prev_slide
 	var next_slide = document.getElementById("next-slide");
 	var prev_slide = document.getElementById("prev-slide");
-	
-	next_slide.onclick = function() { 
+
+	next_slide.onclick = function() {
 		next();
 		clearInterval(idSet);
 		idSet = setInterval('next()', 3000);
 	}
- 
+
 	prev_slide.onclick = function() {
 		previous();
 		clearInterval(idSet);
@@ -113,6 +159,6 @@ function slider() {
 
 /*==================== End slider() function ====================*/
 
-window.onload = function() { 
+window.onload = function() {
 	slider();
 }
