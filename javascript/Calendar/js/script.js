aa79c5b6f46e2cc1859ae15c23788333f9ebbcfb
stoai <dinhtoai94@gmail.com>
2016-03-28 13:09:57 +0700

    update Calendar v2

--- javascript/Calendar/js/script.js
+++ javascript/Calendar/js/script.js
@@ -1,152 +1,243 @@
-days = ["SUN", "MON", "TUE", "WED", "THU", "FRI", "SAT"]; 
-months = ["JAN", "FEB", "MAR", "APR", "MAY", "JUN", "JUL", "AUG", "SEP", "OCT", "NOV", "DEC"]; 
-months_length = new Array("31", "28", "31", "30", "31", "30", "31", "31", "30", "31", "30", "31"); 
-var d = new Date(); 
-var mon = d.getMonth(); 
-var year = d.getYear() + 1900; 
+ days = ["SUN", "MON", "TUE", "WED", "THU", "FRI", "SAT"];
+ months = ["JAN", "FEB", "MAR", "APR", "MAY", "JUN", "JUL", "AUG", "SEP", "OCT", "NOV", "DEC"];
+ months_length = ["31", "28", "31", "30", "31", "30", "31", "31", "30", "31", "30", "31"];
 
-function fillTable() {
-	document.write("<div id = 'main' style = 'display:none; '>"); 
-	document.write("<table border = '1'><tr>"); 
-	document.write("<td><button type = 'button' onclick = 'prevAction(); '>Prev</button</td>"); 
-	document.write("<td id = 'td_mon' colspan = '2'></td>"); 
-	document.write("<td id = 'td_year' colspan = '3'></td>"); 
-	document.write("<td><button type = 'button' onclick = 'nextAction(); '>Next</button</td>"); 
-	document.write("</tr>"); 
-	printWeekDay(); 
-	createDateTable(); 
-	document.write("</table>"); 
+ DAY = new Date();
+ MONTH = DAY.getMonth();
+ YEAR = DAY.getYear() + 1900;
+
+/**
+* The initTable() function creates a table (frame) to show calendar.
+*
+*/
+
+function initCalendar() {
+	document.write("<div id = 'main' style = 'display:none; '>");
+	document.write("<table border = '2'>");
+	document.write("<tr>");
+	//
+	document.write("<td><a href='#' onclick= 'prevYear();'>&larr;</a></td>");
+	document.write("<td><button type = 'button' onclick = 'prevMonth();'>Prev</button</td>");
+	document.write("<td id = 'id_month' colspan = '2'></td>");
+	document.write("<td id = 'id_year' colspan = '1'></td>");
+	document.write("<td><button type = 'button' onclick = 'nextMonth();'>Next</button</td>");
+	//
+	document.write("<td><a href='#' onclick= 'nextYear();'>&rarr;</a></td>");
+	document.write("</tr>");
+	setWeekDay();
+	createDateTable();
+	document.write("</table>");
 	document.write("</div>")
-	setDateContent(mon, year); 
+	setDateContent(MONTH, YEAR);
 }
 
-function buildMonthList() {
-	var res ="<select id = 'ddl_month' onchange = 'setMonth(); '>"; 
-	for(i = 0;  i < months.length;  i++) {
-		if(i == mon) {
-			res  +="<option slected = 'selected' value =" + i +">" + months[i] +"</option>";
+/**
+* The setMonthList() function is set dropdown-list of months.
+*
+*/
+
+function setMonthList() {
+	var res = "<select id = 'selected_month' onchange = 'setMonth();' >";
+	for (i = 0;  i < months.length;  i++) {
+		if (i == MONTH) {
+			res  += "<option selected = 'selected' value ="+ i +">" + months[i] +"</option>";
 		}else {
-			res  +="<option value =" + i +">" + months[i] +"</option>";	
+			res  += "<option value ="+ i +">"+ months[i] +"</option>";
 		}
 	}
-	res +="</select>";
-	return res; 
-}
-function buildYearList() {
-	var res ="<select id = 'ddl_year' onChange = 'setYear(); '>"; 
-	for (i = 1950; i <=  2015; i++) {
-		if (year == i) {
-			res  +="<option selected = 'selected' value =" + i  +">" + i +"</option>";	
-		}else {
-			res  +="<option value =" + i +">" + i +"</option>";
-		}
-	}
-	res +="</select>"; 
-	return res; 
+	res += "</select>";
+	return res;
 }
 
-function printWeekDay() {
-	document.write("<tr>"); 
-	for (i = 0; i<days.length; i++) {
-		document.write("<td class = 'day'>"); 
-		document.write(days[i]); 
-		document.write("</td>"); 
+/**
+* The setYearList() function is set dropdown-list of years.
+*
+*/
+
+function setYearList() {
+	var res = "<select id = 'selected_year' onchange = 'setYear();' >";
+	for (i = 1950; i <=  2097; i++) {
+		if (YEAR == i) {
+			res  += "<option selected = 'selected' value =" + i +">" + i +"</option>";
+		}else {
+			res  += "<option value =" + i +">" + i +"</option>";
+		}
 	}
-	document.write("</tr>"); 
+	res += "</select>";
+	return res;
+
 }
+
+/**
+* The setWeekDay() function is prints weekday from Sunday(0) to Saturday(6).
+*
+*/
+
+function setWeekDay() {
+	document.write("<tr>");
+	for (i = 0; i < days.length; i++) {
+		document.write("<th class = 'day'>");
+		document.write(days[i]);
+		document.write("</th>");
+	}
+	document.write("</tr>");
+}
+
+/**
+* The createDateTable() function is prints a table includes 5 rows and 6 columns to holds dates in month.
+*
+*/
 
 function createDateTable() {
-	var count = 0; 
+	var index = 0;
 	for (i = 0; i < 6; i++) {
-		document.write("<tr>"); 
-		for(j = 0; j < 7; j++) {
-			document.write("<td><a href = '#' id = 'cell" + count +"' onclick = 'onClickDate(this)'></a></td>"); 
-			count++; 
+		document.write("<tr>");
+		for (j = 0; j < 7; j++) {
+			document.write("<td><a href ='#' id ='cell" + index +"' onclick = 'onClickDate(this)'></a></td>");
+			index++;
 		}
-		document.write("</tr>")
+		document.write("</tr>");
 	}
 }
-/*
-* 
+/**
+* The setDateContent() function is full-filled dates in month and show currently date (red).
+*
 */
-function setDateContent(mon, year) {
-	
-	//-----------REFRESH DROPDOWNLIST
+function setDateContent(MONTH, YEAR) {
 
-	document.getElementById("td_mon").innerHTML = buildMonthList(); 
-	document.getElementById("td_year").innerHTML = buildYearList(); 
-	
-	var first_date = new Date(year, mon, 1); 
-	var of_day = first_date.getDay(); 
-	var mon_length = months_length[mon]; 
-	var check = false; 
-	var start = 1; 
+	document.getElementById("id_month").innerHTML = setMonthList();
+	document.getElementById("id_year").innerHTML = setYearList();
 
-	var today = new Date(); 
-	var td = today.getDate(); 
-	var ty = today.getYear() + 1900; 
-	var tm = today.getMonth(); 
-	
+	var first_date = new Date(YEAR, MONTH, 1);
+	var of_day = first_date.getDay();
+	var mon_length = months_length[MONTH];
+	var check = false;
+	var start = 1;
 
-	for(i = 0; i < 42; i++) {
-		if(i == of_day) 
-			check = true; 
-		if(check == true && start <=  mon_length) {
-			if(td == start && tm == mon && ty == year) {
-				document.getElementById("cell" + i).innerHTML = start; 
-				document.getElementById("cell" + i).style.color = "red"; 
+	var today = new Date();
+	var currentDay = today.getDate();
+	var currentYear = today.getYear() + 1900;
+	var currentMonth = today.getMonth();
+
+	for (i = 0; i < 42; i++) {
+		if (i == of_day) {
+			check = true;
+		}
+		if (check == true && start <=  mon_length) {
+			if (currentDay == start && currentMonth == MONTH && currentYear == YEAR) {
+				document.getElementById("cell" + i).innerHTML = start;
+				document.getElementById("cell" + i).style.color = "red";
+			}else {
+				document.getElementById("cell" + i).innerHTML = start;
+				document.getElementById("cell" + i).style.color = "#2582BE";
 			}
-			else {
-				document.getElementById("cell" + i).style.color = "#2582BE"; 
-				document.getElementById("cell" + i).innerHTML = start; 
-			}
-			start++; 
+			start++;
 		}else {
 			document.getElementById("cell" + i).innerHTML = "";
 		}
 	}
 }
 
+/**
+* The setYear() function is set selected year and display corresponding dates in month, selected year.
+*
+*/
+
 function setYear() {
-	var e = document.getElementById("ddl_year"); 
-	year = e.options[e.selectedIndex].value; 
-	setDateContent(mon, year); 
+	var element = document.getElementById("selected_year");
+	YEAR = element.options[element.selectedIndex].value;
+	setDateContent(MONTH, YEAR);
 }
+
+/**
+* The setMonth() function is set selected month and display dates in selected month.
+*
+*/
 
 function setMonth() {
-	var e = document.getElementById("ddl_month"); 
-	mon = e.options[e.selectedIndex].value; 
-	setDateContent(mon, year); 
+	var element = document.getElementById("selected_month");
+	MONTH = element.options[element.selectedIndex].value;
+	setDateContent(MONTH, YEAR);
 }
 
-function nextAction() {
-	if (mon >= 0 && mon < 11) {
-		mon++;
+/**
+* The nextMonth() function is move to next month.
+*
+*/
+
+function nextMonth() {
+	if (MONTH >= 0 && MONTH < 11) {
+		MONTH++;
 	}else {
-		mon = 0;
+		MONTH = 0;
+		YEAR++;
 	}
-	setDateContent(mon, year); 
+	setDateContent(MONTH, YEAR);
 }
 
-function prevAction() {
-	if (mon <=  11 && mon > 0) {
-		mon--;
+/**
+* The nextYear() function is move to next year.
+*
+*/
+
+function nextYear() {
+	if (YEAR < 2097) {
+		YEAR++;
 	}
-	else {
-		mon = 11;
-	}
-	setDateContent(mon, year); 
+	setDateContent(MONTH, YEAR);
 }
 
-function onClickDate(a) {
-	var str = a.innerHTML  + "/" + (parseInt(mon) + 1) + "/" + year; 
-	document.getElementById("input").value = str; 
-	document.getElementById("main").style.display = "none"; 
+/**
+* The prevMonth() function is move to previous month.
+*
+*/
+
+function prevMonth() {
+	if (MONTH <=  11 && MONTH > 0) {
+		MONTH--;
+	}else {
+		MONTH = 11;
+		YEAR--;
+	}
+	setDateContent(MONTH, YEAR);
 }
+
+/**
+* The prevYear() function is move to previous year.
+*
+*/
+
+function prevYear() {
+	if (YEAR > 1950) {
+		YEAR--;
+	}
+	setDateContent(MONTH, YEAR);
+}
+
+/**
+* The onClickDate() function to display information of selected date (date/month/year) into input tag.
+*
+*/
+
+function onClickDate(ob) {
+	var info = ob.innerHTML + " - " + (parseInt(MONTH) + 1) + " - " + YEAR;
+	document.getElementById("input").value = info;
+	document.getElementById("main").style.display = "none";
+}
+
+/**
+* The onClickCal() function to display calendar.
+*
+*/
 
 function onClickCal() {
-	document.getElementById("main").style.display = "block"; 
+	document.getElementById("main").style.display = "block";
 }
 
-window.onload = fillTable();
+/**
+* Invoked initTable() function and refresh tag input when load page.
+*
+*/
+
+window.onload = initCalendar();
 document.getElementById("input").value = "";
