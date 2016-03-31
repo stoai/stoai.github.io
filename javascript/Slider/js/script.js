6cba6bf9163f870127dd45f64f8d16897dfd40a9
stoai <dinhtoai94@gmail.com>
2016-04-01 00:02:43 +0700

    update Slider v5

--- javascript/Slider/js/script.js
+++ javascript/Slider/js/script.js
@@ -45,7 +45,9 @@ function autoSlider() {
 function onTrack(ob){
 	var track = ob.id;
 	currentImage = track.substr(5,1);
+	clearInterval(auto);
 	showImage(currentImage);
+	auto = setInterval('nextImage()', 3000);
 } 
 
 /**
@@ -55,10 +57,10 @@ function onTrack(ob){
 function showImage(track) {
 	for(i = 1; i <= 7; i++) {
 		if (i == track) {
-			document.getElementById("item" + i).style.display = "block";
+			document.getElementById("image" + i).style.display = "block";
 			document.getElementById("index" + i).style.background = "red";
 		} else {
-			document.getElementById("item" + i).style.display = "none";
+			document.getElementById("image" + i).style.display = "none";
 			document.getElementById("index" + i).style.background = "green";
 		}
 	}
