8868a05fb5673080259b23773804bc14ef1952a5
stoai <dinhtoai94@gmail.com>
2016-03-31 08:31:59 +0700

    update Slider v4

--- javascript/Slider/js/script.js
+++ javascript/Slider/js/script.js
@@ -1,54 +1,71 @@
-count = 7;
-currentItem = 1;
+index = 7;
+currentImage = 1;
 /**
-*
+*The prevImage() function is move to previous image.
 *
 */
-function prevAction() {
-	if (currentItem > 1) {
-		currentItem--;
+function prevImage() {
+	if (currentImage > 1) {
+		currentImage--;
 	} else {
-		currentItem = 7;
+		currentImage = 7;
 	}
-	showSlide(currentItem);
+	clearInterval(auto);
+	showImage(currentImage);
+	auto = setInterval('nextImage()', 3000);
 }
 
 /**
-*
+*The nextImage() function is move to next image.
 *
 */
-function nextAction() {
-	if (currentItem < count) {
-		currentItem++;
+function nextImage() {
+	if (currentImage < index) {
+		currentImage++;
 	} else {
-		currentItem = 1;
+		currentImage = 1;
 	}
-	showSlide(currentItem);
+	clearInterval(auto);
+	showImage(currentImage);
+	auto = setInterval('nextImage()', 3000);
 }
 
 /**
-*
+*The autoSlider() function is move to next image automatically.
 *
 */
-function onClickIndex(a){
-	var str = a.id;
-	currentItem = str.substr(5,1);
-	showSlide(currentItem);
+function autoSlider() {
+	auto = setInterval('nextImage()', 3000);
+}
+
+/**
+*The onTrack() function is tracked image.
+*
+*/
+function onTrack(ob){
+	var track = ob.id;
+	currentImage = track.substr(5,1);
+	showImage(currentImage);
 } 
 
 /**
-*
+*The showImage() function is show image in slider.
 *
 */
-function showSlide(index) {
+function showImage(track) {
 	for(i = 1; i <= 7; i++) {
-		if (i == index) {
+		if (i == track) {
 			document.getElementById("item" + i).style.display = "block";
 			document.getElementById("index" + i).style.background = "red";
 		} else {
 			document.getElementById("item" + i).style.display = "none";
 			document.getElementById("index" + i).style.background = "green";
 		}
-		
 	}
 }
+
+/**
+*Invoked autoSlider() function when load page.
+*
+*/
+window.onload = autoSlider();
